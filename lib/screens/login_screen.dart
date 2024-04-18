a6234820ad126b79d7e5d022b00b810215f20e41
thenameisRushi <154388804+thenameisRushi@users.noreply.github.com>
2024-04-17 22:52:40 -0400

    feat:LogIn Functionality added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:job_recuter/screens/home_screen.dart';
 import 'package:job_recuter/screens/register_screen.dart';
 
+
 class LoginScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   // Method to sign in with email and password
   Future<void> signInWithEmailAndPassword(BuildContext context) async {
     try {
+      // Check if email and password are not empty
+      if (emailController.text.trim().isEmpty ||
+          passwordController.text.trim().isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Please enter both email and password.'),
+        ));
+        return;
+      }
+
+      // Sign in with email and password
+      UserCredential userCredential =
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      // Navigate to the bottom navigation screen after successful login
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
             builder: (context) => HomeScreen()),
       );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('No user found for that email.'),
+        ));
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Wrong password provided for that user.'),
+        ));
+      } else if (e.code == 'invalid-email') {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Invalid email format.'),
+        ));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Error signing in. Please try again later.'),
+        ));
+      }
     } catch (e) {
-      print("Error");
+      // Handle other exceptions
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error signing in. Please try again later.'),
+      ));
     }
   }
 
@@ -32,6 +74,7 @@ class LoginScreen extends StatelessWidget {
                 children: [
                   // Email text field
                   TextFormField(
+                    controller: emailController,
                     decoration: InputDecoration(
                       labelText: 'Email',
                       border: OutlineInputBorder(),
@@ -40,6 +83,7 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(height: 16),
                   // Password text field
                   TextFormField(
+                    controller: passwordController,
                     decoration: InputDecoration(
                       labelText: 'Password',
                       border: OutlineInputBorder(),
